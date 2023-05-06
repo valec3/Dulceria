01d971faa1533e61a61be6ab020232c403a7b3db
Victor Raul <v1c70rotakmer@gmail.com>
2023-05-06 01:00:40 -0500

    Terminar seccion inicio

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ const navbarBtn = document.querySelector("#menu-bar")
 
 searchBtn.addEventListener("click",function(e){
     search.classList.toggle("active");
+    searchBtn.classList.toggle("fa-bounce");
 })
 navbarBtn.addEventListener("click",function(){
     navbar.classList.toggle("active")
+    navbarBtn.classList.toggle("fa-times");
+})
+
+window.addEventListener("scroll",(event)=>{
+    // Remove menu scrolling
+    navbar.classList.remove("active"); //Menu hamburguesa
+    navbarBtn.classList.remove("fa-times");
 })
