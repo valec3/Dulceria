0e407745266f2350b43d52365cdf599c15ff4b95
Victor Raul <v1c70rotakmer@gmail.com>
2023-05-16 11:49:27 -0500

    Terminar la seccion blogs

--- js/main.js
+++ js/main.js
@@ -18,3 +18,33 @@ window.addEventListener("scroll",(event)=>{
     navbar.classList.remove("active"); //Menu hamburguesa
     navbarBtn.classList.remove("fa-times");
 })
+
+
+const swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 1,
+        },
+    },
+});
